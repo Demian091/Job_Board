f8503dc5f5f3a4c03b6205f4b58408e2597628ef
Demian091 <123770496+Demian091@users.noreply.github.com>
2025-12-14 20:29:47 +0100

    Update MEDIA_URL

--- jobBoard/settings.py
+++ jobBoard/settings.py
@@ -148,9 +148,9 @@ STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = BASE_DIR / 'media'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
