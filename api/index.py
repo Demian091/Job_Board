77d074eed70eebfc1d8cff15bdd3d366a185e0e5
gamerzkin <uzomae145@gamil.com>
2025-12-14 01:49:05 +0100

    Add WSGI entrypoint for Vercel deployment

--- api/index.py
+++ api/index.py
@@ -1,11 +1,11 @@
 import os
 import sys
 
-from django.core.wsgi import get_wsgi_application
+sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
-# Add project path
-sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "jobBoard.settings")
 
-application = get_wsgi_application()
+from django.core.wsgi import get_wsgi_application
+
+app = get_wsgi_application()
