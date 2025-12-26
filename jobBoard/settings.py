ec1c8b197e4ad1c9a6d172b8b9bd1c19434b1610
Demian091 <123770496+Demian091@users.noreply.github.com>
2025-12-26 13:33:43 +0100

    Integrate Cloudinary configuration in settings
    Added Cloudinary configuration to settings.py.

--- jobBoard/settings.py
+++ jobBoard/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 
 from pathlib import Path
 import os
+import cloudianry 
 from decouple import config
 import dj_database_url
 from datetime import timedelta
@@ -19,7 +20,7 @@ from datetime import timedelta
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-
+cloudinary.config()
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
