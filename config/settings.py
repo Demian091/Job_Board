4470d1469da98f2a430eaf4fbefc14d7dbc18079
Demian091 <123770496+Demian091@users.noreply.github.com>
2026-08-06 23:41:33 +0100

    Add STATICFILES_DIRS for static file management
    Added STATICFILES_DIRS to include static directory.

--- config/settings.py
+++ config/settings.py
@@ -136,6 +136,7 @@ USE_I18N = True
 USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
+STATICFILES_DIRS = [ BASE_DIR / "static",]
 STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
